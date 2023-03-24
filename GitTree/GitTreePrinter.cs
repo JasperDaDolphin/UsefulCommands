2f4908ec6247f126008c168063da3bc4135613e0
JasperDaDolphin <alex@mitrakis.net>
2023-03-24 12:18:08 +1100

    depth, hidefiles, showfilecount

--- GitTree/GitTreePrinter.cs
+++ GitTree/GitTreePrinter.cs
@@ -5,25 +5,40 @@ namespace GitTree;
 
 public class GitTreePrinter
 {
-    private const string ENDBRANCH = "└───";
-    private const string MIDDLEBRANCH = "├───";
-    private const string LINE = "│   ";
-    private const string SPACE = "    ";
+    private const string SPACE          = "    ";
+    private const string LINE           = "│   ";
+    private const string MIDDLEBRANCH   = "├───";
+    private const string ENDBRANCH      = "└───";
 
     private readonly ILogger logger;
 
-    public GitTreePrinter(ILogger logger)
+    private string root;
+    private string path;
+    private int maxDepth;
+    private bool hideFiles;
+    private bool showFileCount;
+
+    public GitTreePrinter(ILogger logger, string root, string path, int maxDepth, bool hideFiles, bool showFileCount)
     {
         this.logger = logger;
+        this.root = root;
+        this.path = path;
+        this.maxDepth = maxDepth;
+        this.hideFiles = hideFiles;
+        this.showFileCount = showFileCount;
     }
 
-    public void Run(string root, string path)
+    public void Run()
     {
         try
         {
             using (var repo = new Repository(root))
             {
-                PrintDirectoryStructure(repo, path, "", true, true);
+                var count = PrintDirectoryStructure(repo, path, "", true, true, 0);
+                if (showFileCount)
+                {
+                    logger.LogInformation($"Count: {count}");
+                }
             }
         }
         catch (RepositoryNotFoundException ex)
@@ -36,32 +51,43 @@ public class GitTreePrinter
         }
     }
 
-    private void PrintDirectoryStructure(Repository repo, string path, string prefix, bool isRoot, bool isLastSibling)
+    private int PrintDirectoryStructure(Repository repo, string path, string prefix, bool isRoot, bool isLastSibling, int currentDepth)
     {
         var dir = new DirectoryInfo(path);
         var subDirs = dir.GetDirectories().Where(a => !IsIgnored(repo, a.FullName)).ToArray();
         var files = dir.GetFiles().Where(a => !IsIgnored(repo, a.FullName)).ToArray();
+        var fileCount = files.Length;
 
         if (!isRoot && dir.Name == ".git")
         {
-            return;
+            return 0;
         }
 
-        logger.LogInformation(prefix + (!isLastSibling ? MIDDLEBRANCH : ENDBRANCH) + dir.Name);
+        logger.LogInformation(prefix + (!isLastSibling ? MIDDLEBRANCH : ENDBRANCH) + dir.Name + (showFileCount ? $" [{fileCount}]" : ""));
         var newPrefix = prefix + (isLastSibling ? SPACE : LINE);
 
-        for (var i = 0; i < files.Length; i++)
+        if (maxDepth != 0 && currentDepth >= maxDepth)
         {
-            var file = files[i];
-            var isCurrentLastSibling = i == files.Length - 1 && subDirs.Length == 0;
-            logger.LogInformation(newPrefix + (!isCurrentLastSibling ? MIDDLEBRANCH : ENDBRANCH) + file.Name);
+            return 0;
+        }
+
+        if (hideFiles)
+        {
+            for (var i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+                var isCurrentLastSibling = i == files.Length - 1 && subDirs.Length == 0;
+                logger.LogInformation(newPrefix + (!isCurrentLastSibling ? MIDDLEBRANCH : ENDBRANCH) + file.Name);
+            }
         }
 
         for (var i = 0; i < subDirs.Length; i++)
         {
             var subDir = subDirs[i];
-            PrintDirectoryStructure(repo, subDir.FullName, newPrefix, false, i == subDirs.Length - 1);
+            fileCount += PrintDirectoryStructure(repo, subDir.FullName, newPrefix, false, i == subDirs.Length - 1, currentDepth + 1);
         }
+
+        return fileCount;
     }
 
     private bool IsIgnored(Repository repo, string path)
--- GitTree/Program.cs
+++ GitTree/Program.cs
@@ -10,16 +10,29 @@ try
     var app = new CommandLineApplication();
     app.HelpOption();
 
-    var root = app.Option("-r|--root <PATH>", "Path of .git directory", CommandOptionType.SingleValue);
+    var root = app.Option("-r|--root <STRING>", "Path of .git directory", CommandOptionType.SingleValue);
     root.DefaultValue = ".";
 
-    var path = app.Option("-p|--path <NAME>", "Path tree to start from", CommandOptionType.SingleValue);
+    var path = app.Option("-p|--path <STRING>", "Path tree to start from", CommandOptionType.SingleValue);
     path.DefaultValue = ".";
+
+    var depth = app.Option<int>("-d|--depth <INT>", "Depth of directories", CommandOptionType.SingleValue);
+    depth.DefaultValue = 0;
+
+    var hideFiles = app.Option<bool>("-hf|--hideFiles <BOOL>", "Hides files", CommandOptionType.NoValue);
+
+    var showFileCount = app.Option<bool>("-sfc|--showFileCount <BOOL>", "Shows file count", CommandOptionType.NoValue);
 
     app.OnExecute(() =>
     {
-        var gitTreePrinter = new GitTreePrinter(logger);
-        gitTreePrinter.Run(root.Value(), path.Value());
+        var gitTreePrinter = new GitTreePrinter(logger, 
+            root.Value(), 
+            path.Value(), 
+            depth.ParsedValue,
+            !hideFiles.ParsedValue,
+            showFileCount.ParsedValue);
+
+        gitTreePrinter.Run();
     });
 
     app.Execute(args);
